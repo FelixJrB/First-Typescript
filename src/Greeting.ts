0be5df21565ffa57511ac8ef742e627a35246463
Felix Berglund <fb222rs@student.lnu.se>
2026-09-07 17:27:15 +0200

    feat: add text output parameters with chalk

--- src/Greeting.ts
+++ src/Greeting.ts
@@ -1,8 +1,14 @@
-class Greeting {
-  private name: string;
+import chalk from 'chalk';
 
-  constructor(name: string){
+class Greeting {
+  private preface: string;
+  private name: string;
+  private phrase: string;
+
+  constructor(name: string, preface: string, phrase: string){
     this.name = name;
+    this.preface = preface;
+    this.phrase = phrase;
   }
   
   get personName(): string {
@@ -14,9 +20,23 @@ class Greeting {
   }
 
   public say(): string {
-    return `Hello, ${this.name}`;
+    return chalk.blue(this.name) + '' + chalk.bold(this.phrase) + '' + chalk.underline(this.preface);
   }
 }
 
-const greeting = new Greeting('Felix');
+const preface = 'Welcome my friend, Im currently testing Typescript';
+console.log(chalk.underline(preface))
+console.log()
+
+
+const forewords = new Greeting('','', chalk.overline`As you read, Im testing Typescript, this is a simple test file`);
+console.log(forewords.say());
+console.log()
+
+const phrase = ''
+const greeting = new Greeting('', phrase,`This is tricky and challeging but also fun.`);
 console.log(greeting.say());
+console.log()
+
+const words = new Greeting('Felix:', phrase, ` Hi, First time in Typescript.`)
+console.log(words.say());
